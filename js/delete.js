f37133677a703fc4384c65d902eaf9946cc9eba3
Jannis R <mail@jannisr.de>
2016-04-28 23:03:51 +0200

    client JS

---
+++ js/delete.js
@@ -0,0 +1,29 @@
+// This is an AJAX workaround because browsers don't support sending DELETE requests using forms.
+// See https://www.w3.org/Bugs/Public/show_bug.cgi?id=10671 and http://amundsen.com/examples/put-delete-forms/
+
+var deleteOnSubmit = function (file, form, button) {
+	form.addEventListener('submit', function (event) {
+		var caption = button.getAttribute('value')
+		var request = new XMLHttpRequest()
+		request.open(form.getAttribute('method'), form.getAttribute('action'))
+		request.addEventListener('readystatechange', function () {
+			if (request.readyState === 4) {
+				file.parentNode.removeChild(file)
+				button.setAttribute('value', caption)
+				button.removeAttribute('disabled')
+			}
+			// todo: ajax failed
+		})
+		request.send()
+		button.setAttribute('value', '…')
+		button.setAttribute('disabled', 'disabled')
+		event.preventDefault()
+	})
+}
+
+Array.from(document.querySelectorAll('#directory #files .file'))
+.forEach(function (file) {
+	var from = file.querySelector('.file-actions-delete')
+	var button = file.querySelector('.file-actions-delete-button')
+	deleteOnSubmit(file, from, button)
+})
